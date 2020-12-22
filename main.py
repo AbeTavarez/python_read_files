ac3b2e6165e25b37fd3482e67550056eef3a64cd
Abraham E. Tavarez <abetavarez@icloud.com>
2020-12-21 23:06:07 -0500

    openpyxl working

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ import os
 workbook = openpyxl.load_workbook('example.xlsx')
 
 print(type(workbook))
+
+sheet = workbook.get_sheet_by_name('Sheet1')
+# print(sheet)
+
+list_sheet = workbook.sheetnames()
+print(list_sheet)
